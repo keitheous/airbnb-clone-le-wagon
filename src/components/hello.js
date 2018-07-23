59184f45e2384ae98ff3093be4933a0ec623e897
keitheous <keithchongwy@gmail.com>
2018-07-23 17:08:16 +1000

    Concept around Components and JSX injection

---
+++ src/components/hello.js
@@ -0,0 +1,14 @@
+// Import react class from react package
+import React from 'react';
+
+// Define class component which extends react.component and returns a render method
+class Hello extends React.Component {
+  render() {
+    // render method returns a html string in JXS
+    return <div>Hello!</div>;
+
+  }
+}
+
+// export component
+export default Hello;
