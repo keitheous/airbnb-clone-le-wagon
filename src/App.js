6abe52b4e72b662db50cc581327acc45acd5e247
keitheous <keithchongwy@gmail.com>
2018-07-25 23:35:18 +1000

    Use Flexbox to implement grid for components

--- src/App.js
+++ src/App.js
@@ -17,7 +17,21 @@ class App extends Component {
 
     return (
       <div>
-        <Flat flat={flat}/>
+        <div className = 'app'>
+
+          <div className = 'main'>
+            <div className = 'search'></div>
+
+            <div className = 'flats'>
+              <Flat flat = {flat}/>
+              <Flat flat = {flat}/>
+              <Flat flat = {flat}/>
+            </div>
+          </div>
+
+          <div className = 'map'>
+          </div>
+        </div>
       </div>
     );
   }
